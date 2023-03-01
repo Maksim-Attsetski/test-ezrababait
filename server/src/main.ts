5d93206eb9e5cab67bd7be5fc1709551ffa285c3
Maksim Attsetski <maks2001maks2001@gmail.com>
2023-03-01 19:25:07 +0300

    conect to DB

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './api/app/app.module';
+
+const PORT = process.env.PORT || 5000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  await app.listen(PORT, () => {
+    console.log(`Server starts on ${PORT} port!`);
+  });
 }
+
 bootstrap();
