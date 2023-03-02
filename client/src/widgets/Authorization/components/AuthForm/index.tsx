2d5259e688986110acce65d9b0c52de09e7c716b
Maksim Attsetski <maks2001maks2001@gmail.com>
2023-03-03 00:05:30 +0300

    add ability to check is tag exist or not

--- client/src/widgets/Authorization/components/AuthForm/index.tsx
+++ client/src/widgets/Authorization/components/AuthForm/index.tsx
@@ -1,5 +1,6 @@
-import { useAuth, useDebounce, useInput } from 'hooks';
+import { useAuth, useDebounce, useInput, useUsers } from 'hooks';
 import React, { FC, FormEvent, memo, useEffect, useState } from 'react';
+import { getApiError } from 'shared';
 import { Button } from 'UI';
 import { ICreateUser, ILoginInfo } from 'widgets/User';
 import s from './AuthForm.module.scss';
@@ -7,34 +8,64 @@ import s from './AuthForm.module.scss';
 const AuthForm: FC = (props) => {
   const [isLogin, setIsLogin] = useState<boolean>(true);
   const { onLogin, onRegistration } = useAuth();
+  const { onGetUsers } = useUsers();
 
-  const { value: tag, setValue: setTag } = useDebounce('');
+  const { value: tag, setValue: setTag } = useDebounce<string>(
+    '',
+    onCheckIsTagExist
+  );
   const email = useInput('', 'E-mail');
   const password = useInput('', 'Password');
   const confirmPassword = useInput('', 'Confirm password');
-
+  const [errorText, setErrorText] = useState<string>('');
   const onFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    if (password.props.value !== confirmPassword.props.value) {
-      throw new Error('Passwords is not equal!');
+    try {
+      if (!isLogin && password.props.value !== confirmPassword.props.value) {
+        throw new Error('Passwords is not equal!');
+      }
+
+      const userForLogin: ILoginInfo = {
+        tag,
+        password: password.props.value,
+      };
+
+      const userForSignup: ICreateUser = {
+        email: email.props.value,
+        name: tag,
+        ...userForLogin,
+      };
+
+      isLogin
+        ? await onLogin(userForLogin)
+        : await onRegistration(userForSignup);
+
+      clearValues();
+    } catch (error) {
+      setErrorText(getApiError(error));
     }
-
-    const userForLogin: ILoginInfo = {
-      tag,
-      password: password.props.value,
-    };
-
-    const userForSignup: ICreateUser = {
-      email: email.props.value,
-      name: tag,
-      ...userForLogin,
-    };
-
-    isLogin ? await onLogin(userForLogin) : await onRegistration(userForSignup);
   };
 
-  useEffect(() => {}, [tag]);
+  const clearValues = () => {
+    const values = { email, password, confirmPassword };
+    for (const key in values) {
+      const element = values[key as keyof typeof values];
+      element.onClear();
+    }
+    setTag('');
+  };
+
+  async function onCheckIsTagExist() {
+    if (isLogin) return;
+
+    const users = await onGetUsers({ filter: 'tag==' + tag }, true);
+    setErrorText(users.length > 0 ? 'This tag is already exist' : '');
+  }
+
+  useEffect(() => {
+    setErrorText('');
+  }, [password.props.value, confirmPassword.props.value]);
 
   return (
     <div className={s.wrapper}>
@@ -46,9 +77,10 @@ const AuthForm: FC = (props) => {
             onChange={(e) => setTag(e.target.value)}
             placeholder='Unique name'
           />
-          <input type='email' {...email.props} />
+          {!isLogin && <input type='email' {...email.props} />}
           <input type='password' {...password.props} />
-          <input type='password' {...confirmPassword.props} />
+          {!isLogin && <input type='password' {...confirmPassword.props} />}
+          <div>{errorText}</div>
           <Button text='Continue' type='submit' />
         </form>
         <div onClick={() => setIsLogin((prev) => !prev)}>
