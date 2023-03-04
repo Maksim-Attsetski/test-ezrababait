a64d19a748064e6b1934b726ad1649e0d61bc267
Maksim Attsetski <maks2001maks2001@gmail.com>
2023-03-04 21:29:21 +0300

    add site build

--- client/src/shared/config.ts
+++ client/src/shared/config.ts
@@ -1,2 +1,3 @@
-export const SERVER_URL = 'http://localhost:8080/';
+export const SERVER_URL = 'https://ezrababait.onrender.com/';
+// export const SERVER_URL = 'http://localhost:8080/';
 export const baseURL = SERVER_URL + 'api/';
